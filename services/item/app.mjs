7e84d3b344f0c98d8a0a36b99d1a60f1c47b26be
CarterCobb <carter.cobb72@gmail.com>
2023-04-11 13:42:18 -0400

    Remove data dir

--- services/item/app.mjs
+++ services/item/app.mjs
@@ -2,7 +2,7 @@ import express from "express";
 import * as nsr from "node-server-router";
 import cors from "cors";
 import cluster from "cluster";
-import os from "os";
+import { availableParallelism } from "node:os";
 import mongoose from "mongoose";
 import Eurika from "./eurika_helper.js";
 
@@ -10,13 +10,13 @@ console.log("penis");
 
 try {
   // if (cluster.isPrimary) {
-  //     for (var _ = 0; _ < os.cpus().length; _++) cluster.fork();
-  //     cluster.on("exit", (worker, _code, _signal) => {
-  //       console.log(`[${worker.process.pid}] Died`);
-  //       cluster.fork();
-  //     });
-  //   } else {
-  //   }
+  //   for (var _ = 0; _ < availableParallelism(); _++) cluster.fork();
+  //   cluster.on("exit", (worker, _code, _signal) => {
+  //     console.log(`[${worker.process.pid}] Died`);
+  //     cluster.fork();
+  //   });
+  //   console.log("running primary");
+  // } else {
   const { json, urlencoded } = express;
   const app = express();
   app.set("trust proxy", true);
@@ -26,7 +26,7 @@ try {
   app.options("*", cors());
   nsr.RouteFactory.applyRoutesTo(app, { log_configured: true });
   mongoose
-    .connect("mongodb://localhost:8989/ItemDB", {
+    .connect("mongodb://mongo:8989", {
       dbName: "ecommerce",
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -35,10 +35,11 @@ try {
   mongoose.connection.on("connected", () =>
     console.log(`[${process.pid}] Connected to MongoDB`)
   );
-  Eurika.registerWithEureka("item", 1000)
+  Eurika.registerWithEureka("item", 1000);
   app.listen(1000, () =>
     console.log(`[${process.pid}] Listening on port ${1000}`)
   );
+  // }
 } catch (error) {
   console.log(error);
 }
