4e447048dc378ebb519250e8e9da61e5334e4933
CarterCobb <carter.cobb72@gmail.com>
2023-04-11 15:40:06 -0400

    formatted

--- services/item/routes/item.js
+++ services/item/routes/item.js
@@ -3,11 +3,13 @@ import { ItemModel } from "../models/item.js";
 
 export default [
   {
-    url: "/items",
+    url: "items",
     action: nsr.HTTPAction.GET,
-    handlers: [async (req, res) => {
+    handlers: [
+      async (req, res) => {
         const items = await ItemModel.find().lean();
         return res.status(200).json(items);
-    }],
+      },
+    ],
   },
 ];
