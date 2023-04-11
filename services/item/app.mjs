84870c8721736b4d584e42fb43676a54ec623cc0
CarterCobb <carter.cobb72@gmail.com>
2023-04-11 14:49:19 -0400

    bug fix

--- services/item/app.mjs
+++ services/item/app.mjs
@@ -1,22 +1,17 @@
 import express from "express";
 import * as nsr from "node-server-router";
 import cors from "cors";
-import cluster from "cluster";
-import { availableParallelism } from "node:os";
 import mongoose from "mongoose";
 import Eurika from "./eurika_helper.js";
 
-console.log("penis");
+const connectToDB = () =>
+  mongoose.connect("mongodb://mongo:27017", {
+    dbName: "ecommerce",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
 try {
-  // if (cluster.isPrimary) {
-  //   for (var _ = 0; _ < availableParallelism(); _++) cluster.fork();
-  //   cluster.on("exit", (worker, _code, _signal) => {
-  //     console.log(`[${worker.process.pid}] Died`);
-  //     cluster.fork();
-  //   });
-  //   console.log("running primary");
-  // } else {
   const { json, urlencoded } = express;
   const app = express();
   app.set("trust proxy", true);
@@ -25,21 +20,15 @@ try {
   app.use(cors());
   app.options("*", cors());
   nsr.RouteFactory.applyRoutesTo(app, { log_configured: true });
-  mongoose
-    .connect("mongodb://mongo:8989", {
-      dbName: "ecommerce",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .catch(console.log);
+  connectToDB().catch(connectToDB);
   mongoose.connection.on("connected", () =>
     console.log(`[${process.pid}] Connected to MongoDB`)
   );
+  mongoose.connection.on("error", (e) => console.log(`[${process.pid}] ${e}`));
   Eurika.registerWithEureka("item", 1000);
   app.listen(1000, () =>
     console.log(`[${process.pid}] Listening on port ${1000}`)
   );
-  // }
 } catch (error) {
   console.log(error);
 }
